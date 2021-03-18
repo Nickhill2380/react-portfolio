6c63f12988c066373e37d46af33a2e584c78601e
Nick Hill <nickhill2380@gmail.com>
2021-03-18 16:47:51 -0400

    added stackoverlow icon and link to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import gitLogo from '../../assets/icons/GitHub-Mark-64px.png';
 import linkedLogo from '../../assets/icons/LI-In-Bug.png';
+import stackLogo from '../../assets/icons/stackoverflow-logo.png'
 
 function footer() {
     return (
@@ -10,7 +11,10 @@ function footer() {
         </p>
 
         <p>
-            <a href="https://www.linkedin.com/in/nick-hill-04357677/"><img src={linkedLogo}className="logo" alt="Linkedin"/></a>
+            <a href="https://www.linkedin.com/in/nick-hill-04357677/"><img src={linkedLogo} className="logo" alt="Linkedin"/></a>
+        </p>
+        <p>
+            <a href="https://stackoverflow.com/users/14336558/nickhill2380"><img src={stackLogo} className="logo" alt="Stack OverFlow" /></a>
         </p>
         </footer>
     )
